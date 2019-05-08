76eb6e2ad665268689f10b996852afd834698704
OszO <oszo_oasis@hotmail.com>
2019-05-09 02:12:42 +0700

    Add dark theme

--- CTFd/views.py
+++ CTFd/views.py
@@ -55,7 +55,7 @@ def setup():
 
             index = """<div class="row">
     <div class="col-md-6 offset-md-3">
-        <img class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
+        <img id="img-logo" class="w-100 mx-auto d-block" style="max-width: 500px;padding: 50px;padding-top: 14vh;" src="themes/core/static/img/logo.png" />
         <h3 class="text-center">
             <p>A cool CTF platform from <a href="https://ctfd.io">ctfd.io</a></p>
             <p>Follow us on social media:</p>
